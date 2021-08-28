b282e307bda489ec7f35c17983fd2294809d31e8
hong <408440435@gms.tku.edu.tw>
2021-08-28 16:18:32 +0800

    for lesson1 add workspace add package add hello_world.cpp

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/daniel/test/src
+# Build directory: /home/daniel/test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("example")
